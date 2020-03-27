10614c4555838e6ca60000dea7124d9dae101ae1
Benjamin Wichser <benwichser@gmail.com>
2020-03-27 21:53:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,13 @@ class Song
     song = self.new  
     song.save
     song 
-    
+  end 
+
+  def self.new_by_name(name)
+    song = self.new 
+    song.name = name 
+    song.save
+    song 
   end 
 
   def self.all
